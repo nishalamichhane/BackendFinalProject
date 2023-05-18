3aaad60d8f8f4e72f6851f7431637851fb8944b0
nishalamichhane <nishalamichhane9@gmail.com>
2023-05-18 11:31:41 +0200

    Testing for set up springboot

---
+++ src/test/java/com/example/BackendFinalProject/BackendFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
